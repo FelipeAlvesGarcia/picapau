8c1f6fbad94466e057aa55511e9b233c9bd8a428
= <=>
2024-11-30 18:42:08 -0300

    troca de renderização

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login/Login.jsx';
+import Cadastro from './pages/Cadastro/Cadastro.jsx'
+import Home from './pages/Home/Home.jsx'
+import Recrutamento from './pages/Recrutamento/Recrutamento.jsx';
+import CriarRecrutamento from './pages/CriarRecrutamento/CriarRecrutamento.jsx';
+import Perfil from './pages/Perfil/Perfil.jsx';
+import Equipe from './pages/Equipe/Equipe.jsx';
+import Lider from './pages/lider/Lider.jsx';
+import RecrutamentoDetalhes from './pages/RecrutamentoDetalhes/RecrutamentoDetalhes.jsx';
+import CadastrarCurriculo from './pages/CadastrarCurruculo/CadastrarCurriculo.jsx';
 
 const App = () => {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <>
+      <Outlet/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/recrutamento" element={<Recrutamento />} />
+          <Route path="/recrutamento/criar" element={<CriarRecrutamento />} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/equipe" element={<Equipe />} />
+          <Route path="/lider" element={<Lider />} />
+          <Route path="/curriculo/cadastrar" element={<CadastrarCurriculo />} />
+          <Route path="/recrutamento/detalhes/:id" element={<RecrutamentoDetalhes />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
-import Login from './pages/Login/Login.jsx';
-import Cadastro from './pages/Cadastro/Cadastro.jsx'
-import Home from './pages/Home/Home.jsx'
-import Recrutamento from './pages/Recrutamento/Recrutamento.jsx';
-import CriarRecrutamento from './pages/CriarRecrutamento/CriarRecrutamento.jsx';
-import Perfil from './pages/Perfil/Perfil.jsx';
-import Equipe from './pages/Equipe/Equipe.jsx';
-import Lider from './pages/lider/Lider.jsx';
-import RecrutamentoDetalhes from './pages/RecrutamentoDetalhes/RecrutamentoDetalhes.jsx';
-import CadastrarCurriculo from './pages/CadastrarCurruculo/CadastrarCurriculo.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />, 
-    children: [
-      {path: "/", element: <Login /> },
-      {path: "/cadastro", element: <Cadastro /> },
-      {path: "/home", element: <Home />},
-      {path: "/recrutamento", element: <Recrutamento />},
-      {path: "/recrutamento/criar", element: <CriarRecrutamento />},
-      {path: "/perfil", element: <Perfil />},
-      {path: "/equipe", element: <Equipe />},
-      {path: "/lider", element: <Lider />},
-      {path: "/curriculo/cadastrar", element: <CadastrarCurriculo />},
-      {path: "/recrutamento/detalhes/:id", element: <RecrutamentoDetalhes />}
-    ]
-  }
-]);
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 );
