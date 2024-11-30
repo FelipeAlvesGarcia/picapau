0ddc80b423bfd77687e837ecc62eefd7232adfe7
= <=>
2024-11-30 19:14:56 -0300

    react-create-app

---
+++ src/pages/CriarRecrutamento/CriarRecrutamento.jsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './CriarRecrutamento.css';
+import Headeri from '../../components/Headeri';
+import {getDados} from "../../components/local.jsx"
+import axios from 'axios';
+
+function CriarRecrutamento (){
+
+  const [nome, setNome] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [curriculos, setCurriculos] = useState(["67476832893fb118482365cf"]); 
+
+  let user = getDados()
+
+  function funCriarRecrutamento(e){
+    e.preventDefault();
+    console.log("Nome: " + nome)
+    console.log("Descricao: " + descricao)
+
+
+    const url = axios.create({
+      baseURL: "https://picapauapi-production.up.railway.app/api",
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      }
+    });
+    url.post("recrutamentos", {
+      nome:nome,
+      descricao:descricao,
+      curriculos:curriculos
+    }).then((resp) => {
+      console.log(resp.data);
+      setNome("");
+      setDescricao("");
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+  //
+
+  const [listaCurriculos, setListaCurriculos] = useState([]);
+
+  useEffect(() => {
+    const httpCurriculos = axios.create({
+      baseURL: "https://picapauapi-production.up.railway.app/api",
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      }
+    })
+    
+    httpCurriculos.get("curriculos/meus-curriculos")
+      .then((response) => {
+        setListaCurriculos(response.data.curriculos)
+        console.log(response.data.curriculos);
+        console.log(listaCurriculos);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log(listaCurriculos);
+  }, [listaCurriculos]);
+
+  return (
+    <>
+      <Headeri />
+      <div className="recruitment-page">
+        <div className="container mt-4">
+          <div className="row">
+            <div className="col-12">
+              <h1 className="recruitment-title">Criar Novo Recrutamento</h1>
+            </div>
+
+            <div className="form-container">
+              <form onSubmit={funCriarRecrutamento}>
+                <div className="form-group">
+                  <label>Nome do Recrutamento</label>
+                  <input
+                    type="text"
+                    name="nome"
+                    value={nome}
+                    onChange={(e) => {setNome(e.target.value)}}
+                    placeholder="Nome do Recrutamento"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Descrição</label>
+                  <textarea
+                    name="descricao"
+                    value={descricao}
+                    onChange={(e) => {setDescricao(e.target.value)}}
+                    placeholder="Descrição"
+                    rows="4"
+                    required
+                  ></textarea>
+                </div>
+                <div>
+                  <h3>Selecione os Curriculos</h3>
+                  {
+                    listaCurriculos.map((cur) => {
+
+                      return(
+                        <div>
+                          
+                        </div>
+                      );
+                    })
+                  }
+                </div>
+                <div>
+                  <button type="submit" className="btn btn-primary">Criar</button>
+                </div>
+              </form>
+            </div>  
+          </div>
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CriarRecrutamento;
